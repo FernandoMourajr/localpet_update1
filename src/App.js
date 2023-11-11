01b7ed5db87b9226a34aa9cc3570a56ff6710970
FernandoMourajr <emersonfernandodemourajunior@gmail.com>
2023-11-11 20:15:48 -0300

    localpet update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <main className="min-h-screen flex flex-col">
-      <header className="flex h-[6rem] bg-yellow-100 shadow-md z-10">
+      <header className="flex h-[6rem] bg-[#FFF7E0] shadow-md z-10">
         <div className="flex flex-1 px-[3.3rem] py-4 items-center justify-between">
           <div className="flex space-x-2 items-center">
             <a>
@@ -28,18 +28,29 @@ function App() {
             <a>
               <img src={images.search} />
             </a>
-        
-            <button className="flex w-22 h-[15.3] bg-slate-50 rounded shadow">
+
+            <button 
+            className="flex w-22 h-[15.3]
+             bg-slate-50 
+             rounded 
+             shadow
+             hover:bg-yellow-50">
+              
               <a href="#" className="flex space-x-[0.4] m-3">
                 <img src={images.profile} alt="Profile" className="flex mr-2" />
                 <span>Entrar</span>
               </a>
-        
             </button>
-              
-            <button className="flex w-22 h-[15.3] bg-slate-50 rounded shadow" >
+
+            <button
+              className="flex w-22 h-[15.3]
+               bg-slate-50
+                rounded 
+                shadow
+               hover:bg-yellow-50 
+                transition">
               <span className="flex m-4 h-5 items-center">Cadastrar</span>
-            </button>  
+            </button>
 
             <a>
               <img src={images.cart} />
@@ -57,11 +68,93 @@ function App() {
           {data.map((item) => (
             <SwiperSlide key={item.id}>
               <div className="flex justify-center">
-                <img src={item.slide} alt="Slider" className="slide-item" />
+                <img src={item.slide} alt="Slider" className="slide-item flex w-full" />
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
+
+        <div className="flex justify-between h-1 py-12 px-20 text-lg">
+          <span className="font-bold m-1">Produtos Recomendados</span>
+          <span>9 Produtos</span>
+        </div>
+
+        <hr className="bg-[#eee]"/>
+
+        <div className="flex justify-between py-5 px-20">
+
+          <div className="flex flex-col gap-10">
+            <div>
+              <a href="#" className="border border-solid border-gray-300 w-72 rounded-lg p-1 text-center flex flex-col items-center">
+                <img src={images.gspFrangoCarne} className="w-44 object-cover rounded-lg"/>
+                <span className="font-semibold text-[#1D1D1D] text-lg">Ração Golden Special - Frango e Carne</span>
+                <span className="text-[#0326a6] text-lg">R$154,90</span>
+              </a>
+            </div>
+            <div>
+              <a href="#" className="border border-solid border-gray-300 w-72 rounded-lg p-1 text-center flex flex-col items-center">
+                <img src={images.premier} className="w-44 object-cover rounded-lg"/>
+                <span className="font-semibold text-[#1D1D1D] text-lg">Ração Premier - Frango</span>
+                <span className="text-[#0326a6] text-lg">R$258,00</span>
+              </a>
+            </div>
+            <div>
+              <a href="#" className="border border-solid border-gray-300 w-72 rounded-lg p-1 text-center flex flex-col items-center">
+                <img src={images.racaoggFrango} className="w-44 object-cover rounded-lg"/>
+                <span className="font-semibold text-[#1D1D1D] text-lg">Ração Golden para Gatos - Sabor Frango</span>
+                <span className="text-[#0326a6] text-lg">R$50,00</span>
+              </a>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-10">
+            <div>
+              <a href="#" className="border border-solid border-gray-300 w-72 rounded-lg p-1 text-center flex flex-col items-center"> 
+                  <img src={images.gspPowerTraining} className="w-44 object-cover rounded-lg"/>
+                  <span className="font-semibold text-[#1D1D1D] text-lg">Ração Golden Power Training - Frango e Arroz</span>
+                  <span className="text-[#0326a6] text-lg">175,00</span>
+                </a>
+              </div>
+              <div>
+              <a href="#" className="border border-solid border-gray-300 w-72 rounded-lg p-1 text-center flex flex-col items-center">
+                  <img src={images.gspFrangoCarne} className="w-44 object-cover rounded-lg"/>
+                  <span className="font-semibold text-[#1D1D1D] text-lg">Ração Origens Class - Carne e Frango</span>
+                  <span className="text-[#0326a6] text-lg">50,00</span>
+                </a>
+              </div>
+              <div>
+                <a href="#" className="border border-solid border-gray-300 w-72 rounded-lg p-1 text-center flex flex-col items-center">
+                  <img src={images.gspFrangoCarne} className="w-44 object-cover rounded-lg"/>
+                  <span className="font-semibold text-[#1D1D1D] text-lg">Ração Golden Special - Frango e Carne</span>
+                  <span className="text-[#0326a6] text-lg">R$50,00</span>
+                </a>
+            </div>
+         </div>
+
+          <div className="flex flex-col gap-10">
+              <div>
+                <a href="#" className="border border-solid border-gray-300 w-72 rounded-lg p-1 text-center flex flex-col items-center">
+                  <img src={images.gseFrangoArroz} className="w-44 object-cover rounded-lg"/>
+                  <span className="font-semibold text-[#1D1D1D] text-lg">Ração Golden Cães Senior - Frango e Arroz</span>
+                  <span className="text-[#0326a6] text-lg">180,00</span>
+                </a>
+                </div>
+                <div>
+                <a href="#" className="border border-solid border-gray-300 w-72 rounded-lg p-1 text-center flex flex-col items-center">
+                  <img src={images.gspFrangoCarne} className="w-44 object-cover rounded-lg"/>
+                  <span className="font-semibold text-[#1D1D1D] text-lg">Ração Origens Class - Carne e Frango</span>
+                  <span className="text-[#0326a6] text-lg">50,00</span>
+                </a>
+                </div>
+                <div>
+                <a href="#" className="border border-solid border-gray-300 w-72 rounded-lg p-1 text-center flex flex-col items-center">
+                  <img src={images.gspFrangoCarne} className="w-44 object-cover rounded-lg"/>
+                  <span className="font-semibold text-[#1D1D1D] text-lg">Ração Golden Special - Frango e Carne</span>
+                  <span className="text-[#0326a6] text-lg">R$50,00</span>
+                </a>
+              </div>
+          </div>
+        </div>
       </section>
     </main>
   );
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,9 +2,14 @@ const cart = require("./cart.png")
 const logo = require("./Logo.png")
 const profile = require("./profile.png")
 const search = require("./Search.png")
-
+const gseFrangoArroz = require("./GoldenSeniorFrangoeArroz.png")
+const gspFrangoCarne = require("./GoldenSpecialfrangoCarne.png")
+const gspPowerTraining = require("./GoldenSpecialPowerTraining.png")
+const premier = require ("./Premier.png")
+const racaoggFrango = require ("./RacaoGoldenGatosFrango.png")
 
 export const images = {
-    cart, logo, profile, search
+    cart, logo, profile, search, gseFrangoArroz, gspFrangoCarne, gspPowerTraining, premier, racaoggFrango
 }
+
 
